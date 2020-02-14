ff389d97a71de0098fbc19c00f8145f3d8395ee5
Alien <leomanangka@outlook.com>
2020-02-14 15:19:26 +0700

    modify Rpi-car

--- Rpi-car.py
+++ Rpi-car.py
@@ -7,10 +7,17 @@ import time
 import struct
 
 class car(object):
-    def __init__(self):
+    def __init__(self, ip, port):
         self.address = 0x04
         self.i2c = smbus.SMBus(1)
 
         self.client = socket.socket()
-        self.client.connect(('192.168.43.216', 1234))
-        self.
+        self.client.connect(('ip', port))
+        self.conn.client.makefile('wb')
+
+    def vision(self):
+        with picamera.Picamera() as camera:
+            camera.rotation = 180
+            camera.resolution = (320, 240)
+            camera.framerate = 15
+            time.sleep(2)
