b33b0ea7357347ae361818d89eb4327ad63d9009
Alien <leomanangka@outlook.com>
2020-02-14 14:58:01 +0700

    modify Rpi-car

--- Rpi-car.py
+++ Rpi-car.py
@@ -5,3 +5,12 @@ import threading
 import io
 import time
 import struct
+
+class car(object):
+    def __init__(self):
+        self.address = 0x04
+        self.i2c = smbus.SMBus(1)
+
+        self.client = socket.socket()
+        self.client.connect(('192.168.43.216', 1234))
+        self.
